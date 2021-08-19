616cbecba168e1914aed3c804f019a92ece627bf
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2021-08-19 10:02:52 -0600

    atributos

--- my-element.js
+++ my-element.js
@@ -2,18 +2,19 @@ class myElement extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({mode: "open"});
+
+		this.title = this.getAttribute("title");
+		this.parrafo = this.getAttribute("parrafo");
+		this.img = this.getAttribute("img");
 	}
 	getTemplate() {
 		const template = document.createElement('template');
 		template.innerHTML = `
 			<section>
-				<h2>
-					<slot name="title"></slot>
-				</h2>
+				<h2>${this.title}</h2>
 				<div>
-					<p>
-					<slot name="paragrafo"></slot>
-					</p>
+					<p>${this.parrafo}</p>
+					<img src="${this.img}"/>
 				</div>
 			</section>
 			${this.getStyles()}
