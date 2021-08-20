c11912b4c31ebd859f4e819a000e6e07fd9826c1
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2021-08-20 09:42:10 -0600

    disconnectedCallback '3'

---
+++ disconnectedCallback/app.js
@@ -0,0 +1,15 @@
+class MyCustomElement extends HTMLElement {
+	constructor() {
+		super();
+		console.log("Hola desde el constructor - Memoria");
+	}
+	connectedCallback() {
+		console.log("hola desde el DOM");
+	}
+	disconnectedCallback() {
+		console.log("Adios del DOM");
+	}
+}
+customElements.define("my-custom-element", MyCustomElement);
+
+document.querySelector("my-custom-element").remove();
